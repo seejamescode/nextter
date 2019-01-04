11960348e4293db8b693c54fcb334b78a8692c46
seejamescode <james@seejamescode.com>
2019-01-04 15:51:01 -0600

    style(performance): use css-in-js to only deliver critical styles

--- components/Comment.js
+++ components/Comment.js
@@ -1,10 +1,21 @@
 import { COMMENT } from "../common-types";
+import styled from "styled-components";
+
+const Container = styled.div`
+  padding: var(--padding) 0;
+`;
+
+const Name = styled.h4`
+  font-size: 1.25rem;
+  font-weight: 400;
+  margin: 0 auto 0.35rem auto;
+`;
 
 const Comment = ({ body, name }) => (
-  <div>
-    <h4>{name}</h4>
+  <Container>
+    <Name>{name}</Name>
     <p>{body}</p>
-  </div>
+  </Container>
 );
 
 Comment.propTypes = COMMENT;
