8bbca1df66283091fa348f90f548acc8d2738ced
seejamescode <james@seejamescode.com>
2019-01-04 16:40:24 -0600

    feat(bonus): social sharing and icon/splash screen for ios devices

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,12 @@
 import Document, { Head, Main, NextScript } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
+const title = "Nextter";
+const description =
+  "A Twitter-like sample app with a perfect Google Lighthouse score.";
+const url = "https://nextter.now.sh";
+const thumbnail = `${url}/static/graphics/thumbnail.png`;
+
 export default class MyDocument extends Document {
   // This snippet will collect all of page’s critical CSS
   // while the is being server-side rendered
@@ -33,6 +39,81 @@ export default class MyDocument extends Document {
           <meta
             name="Description"
             content="A Twitter-like sample app with a perfect Google Lighthouse score."
+          />
+
+          {/* Bonus: Have beautiful preview tiles when users share your website on social media */}
+          <meta property="og:title" content={title} />
+          <meta property="og:description" content={description} />
+          <meta property="og:image" content={thumbnail} />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content={url} />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:title" content={title} />
+          <meta name="twitter:description" content={description} />
+          <meta name="twitter:image" content={thumbnail} />
+
+          {/* Bonus: Have app icon and splash screen for PWAs saved to homescreen on iOS devices */}
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+          <link
+            rel="apple-touch-icon"
+            sizes="57x57"
+            href="./static/graphics/icon-57.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="72x72"
+            href="./static/graphics/icon-72.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="114x114"
+            href="./static/graphics/icon-114.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="144x144"
+            href="./static/graphics/icon-144.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="512x512"
+            href="./static/graphics/icon-512.png"
+          />
+          <link
+            href="./static/graphics/splash-2048.png"
+            media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)"
+            rel="apple-touch-startup-image"
+          />
+          <link
+            href="./static/graphics/splash-1668.png"
+            media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)"
+            rel="apple-touch-startup-image"
+          />
+          <link
+            href="./static/graphics/splash-1536.png"
+            media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)"
+            rel="apple-touch-startup-image"
+          />
+          <link
+            href="./static/graphics/splash-1125.png"
+            media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)"
+            rel="apple-touch-startup-image"
+          />
+          <link
+            href="./static/graphics/splash-1242.png"
+            media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3)"
+            rel="apple-touch-startup-image"
+          />
+          <link
+            href="./static/graphics/splash-750.png"
+            media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)"
+            rel="apple-touch-startup-image"
+          />
+          <link
+            href="./static/graphics/splash-640.png"
+            media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)"
+            rel="apple-touch-startup-image"
           />
         </Head>
         <body>
